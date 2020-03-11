01396cda9789ac328815d77965c859b8b29e42af
talentdevguru <55609519+talentdevguru@users.noreply.github.com>
2020-03-10 23:44:11 -0700

    asdf

--- routes/sendmails.js
+++ routes/sendmails.js
@@ -1,4 +1,4 @@
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
